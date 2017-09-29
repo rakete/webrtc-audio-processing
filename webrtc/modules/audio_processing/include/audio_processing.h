876e1398cbe7a1f7f33ae5049c42f1c3986c8f99
henno <henno@binaerwerkstatt.de>
2017-09-29 13:59:28 +0200

    minor changes for suppressing -Wunused_paramater compile output

--- webrtc/modules/audio_processing/include/audio_processing.h
+++ webrtc/modules/audio_processing/include/audio_processing.h
@@ -415,7 +415,10 @@ class AudioProcessing {
   // Same as above but uses an existing PlatformFile handle. Takes ownership
   // of |handle| and closes it at StopDebugRecording().
   // TODO(xians): Make this interface pure virtual.
-  virtual int StartDebugRecordingForPlatformFile(rtc::PlatformFile handle) {
+  virtual int StartDebugRecordingForPlatformFile(rtc::PlatformFile handle)
+  {
+  
+      if ((int)handle == 0) { return -1; } // prevent unsued parameter gcc warning
       return -1;
   }
 
